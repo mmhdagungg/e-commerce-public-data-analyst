375fa88909d974c2ccca64505be800fb642fe131
mmhdagungg <mmhdagung7@gmail.com>
2024-10-04 22:58:13 +0700

    add link

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -10,12 +10,12 @@ sns.set(style='dark')
 
 # Dataset
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-all_df = pd.read_csv("https://raw.githubusercontent.com/mmhdagungg/e-commerce-publict-data-analysis/refs/heads/main/dashboard/df.csv")
+all_df = pd.read_csv("https://raw.githubusercontent.com/mmhdagungg/e-commerce-public-data-analyst/refs/heads/main/dashboard/df.csv")
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
 
 # Data Geolocation
-geolocation = pd.read_csv("https://raw.githubusercontent.com/mmhdagungg/e-commerce-publict-data-analysis/refs/heads/main/dashboard/geolocation.csv")
+geolocation = pd.read_csv("https://raw.githubusercontent.com/mmhdagungg/e-commerce-public-data-analyst/refs/heads/main/dashboard/geolocation.csv")
 data = geolocation.drop_duplicates(subset='customer_unique_id')
 
 for col in datetime_cols:
