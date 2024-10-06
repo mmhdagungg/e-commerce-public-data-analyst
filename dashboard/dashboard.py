370380bc7dbbcc70fab2a4dd83c6c77f5b32e7ce
mmhdagungg <mmhdagung7@gmail.com>
2024-10-06 13:59:23 +0700

    changes notebook

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -55,130 +55,55 @@ state, most_common_state = function.create_bystate_df()
 order_status, common_status = function.create_order_status()
 
 # Judul
-st.title("E-Commerce Public Data Analysis")
+st.title("E-Commerce Data Analysis")
 
-# delivered order by daily
-st.subheader("Daily Orders Delivered")
-col1, col2 = st.columns(2)
+order_items_df = pd.read_csv("https://raw.githubusercontent.com/mmhdagungg/e-commerce-public-data-analyst/refs/heads/main/dashboard/df.csv")
 
-with col1:
-    total_order = daily_orders_df["order_count"].sum()
-    st.markdown(f"Total Order: **{total_order}**")
+sum_order_items_df = order_items_df.groupby('product_category_name_english').agg(
+    product_count=('order_item_id', 'count')
+).reset_index()
 
-with col2:
-    total_revenue = daily_orders_df["revenue"].sum()
-    st.markdown(f"Total Revenue: **{total_revenue}**")
+total_items = sum_order_items_df["product_count"].sum()
+avg_items = sum_order_items_df["product_count"].mean()
 
-fig, ax = plt.subplots(figsize=(12, 6))
-sns.lineplot(
-    x=daily_orders_df["order_approved_at"],
-    y=daily_orders_df["order_count"],
-    marker="o",
-    linewidth=2,
-    color="#ebb734"
-)
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
-st.pyplot(fig)
+st.markdown(f"**Total Items:** {total_items}")
+st.markdown(f"**Rata-rata Items:** {avg_items}")
 
-# Customer Spend Money
-st.subheader("Customer Spend Money")
-col1, col2 = st.columns(2)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(18, 10))
 
-with col1:
-    total_spend = sum_spend_df["total_spend"].sum()
-    st.markdown(f"Total Spend: **{total_spend}**")
-
-with col2:
-    avg_spend = sum_spend_df["total_spend"].mean()
-    st.markdown(f"Average Spend: **{avg_spend}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-sns.lineplot(
-    data=sum_spend_df,
-    x="order_approved_at",
-    y="total_spend",
-    marker="o",
-    linewidth=2,
-    color="#ebb734"
-)
-
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
-st.pyplot(fig)
-
-# Order Items
-st.subheader("Order Items")
-col1, col2 = st.columns(2)
-
-with col1:
-    total_items = sum_order_items_df["product_count"].sum()
-    st.markdown(f"Total Items: **{total_items}**")
-
-with col2:
-    avg_items = sum_order_items_df["product_count"].mean()
-    st.markdown(f"Average Items: **{avg_items}**")
-
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
-
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette="coolwarm", ax=ax[0])
+# Penjualan terbanyak
+top_sales = sum_order_items_df.sort_values(by="product_count", ascending=False).head(5)
+sns.barplot(x="product_count", y="product_category_name_english", 
+            data=top_sales, palette="coolwarm", ax=ax[0])
 ax[0].set_ylabel(None)
-ax[0].set_xlabel("Number of Sales", fontsize=80)
-ax[0].set_title("Most sold products", loc="center", fontsize=90)
-ax[0].tick_params(axis ='y', labelsize=55)
-ax[0].tick_params(axis ='x', labelsize=50)
+ax[0].set_xlabel("Number of Sales", fontsize=14)
+ax[0].set_title("Penjualan Terbanyak", loc="center", fontsize=16)
+ax[0].tick_params(axis='y', labelsize=12)
+ax[0].tick_params(axis='x', labelsize=12)
 
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette="coolwarm", ax=ax[1])
+# Penjualan terendah
+sns.barplot(x="product_count", y="product_category_name_english", 
+            data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette="coolwarm", ax=ax[1])
 ax[1].set_ylabel(None)
-ax[1].set_xlabel("Number of Sales", fontsize=80)
+ax[1].set_xlabel("Number of Sales", fontsize=14)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position("right")
 ax[1].yaxis.tick_right()
-ax[1].set_title("Fewest products sold", loc="center", fontsize=90)
-ax[1].tick_params(axis='y', labelsize=55)
-ax[1].tick_params(axis='x', labelsize=50)
+ax[1].set_title("Penjualan Terendah", loc="center", fontsize=16)
+ax[1].tick_params(axis='y', labelsize=12)
+ax[1].tick_params(axis='x', labelsize=12)
 
 st.pyplot(fig)
+with st.expander("Lihat Penjelasan"):
+        st.write('Produk terlaris adalah audio dan terendah adalah security dan services')
 
-# Review Score
-st.subheader("Review Score")
-col1, col2 = st.columns(2)
-
-with col1:
-    avg_review_score = review_score.mean()
-    st.markdown(f"Average Review Score: **{avg_review_score:.2f}**")
-
-with col2:
-    most_common_review_score = review_score.value_counts().idxmax()
-    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-colors = sns.color_palette("coolwarm", len(review_score))
-
-sns.barplot(x=review_score.index,
-            y=review_score.values,
-            order=review_score.index,
-            palette=colors)
-
-plt.title("Customer Review Scores for Service", fontsize=15)
-plt.xlabel("Rating")
-plt.ylabel("Count")
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-
-# Menambahkan label di atas setiap bar
-for i, v in enumerate(review_score.values):
-    ax.text(i, v + 5, str(v), ha='center', va='bottom', fontsize=12, color='black')
-
-st.pyplot(fig)
-
-# Customer Demographic
-st.subheader("Customer Demographic")
+# Demografi Pelanggan
+st.subheader("Demografi Pelanggan")
 tab1, tab2 = st.tabs(["State", "Geolocation"])
 
 with tab1:
     most_common_state = state.customer_state.value_counts().index[0]
-    st.markdown(f"Most Common State: **{most_common_state}**")
+    st.markdown(f"State dengan customer terbanyak: **{most_common_state}**")
 
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.barplot(x=state.customer_state.value_counts().index,
@@ -187,16 +112,17 @@ with tab1:
                 palette="coolwarm"
                     )
 
-    plt.title("Number customers from State", fontsize=15)
+    plt.title("Data Pelanggan berdasarkan State", fontsize=15)
     plt.xlabel("State")
-    plt.ylabel("Number of Customers")
+    plt.ylabel("Jumlah Pelanggan")
     plt.xticks(fontsize=12)
     st.pyplot(fig)
+
+    with st.expander("Lihat Penjelasan"):
+        st.write("Berdasarkan data, jumlah customer paling banyak berada di state SP (Sao Paulo) dan terendah berada di state RR (Roraima)")
 
 with tab2:
     map_plot.plot()
 
-    with st.expander("See Explanation"):
+    with st.expander("Lihat Penjelasan"):
         st.write('Pelanggan lebih banyak berada di Sao Paulo, Rio De Janiero, dll')
-
-st.caption('Copyright (C) Moehammad Agung Putra Darmawan')
